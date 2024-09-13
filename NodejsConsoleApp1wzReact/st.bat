f4e9355141e7ad0aa48887e76fc9a14e28554c1b
ati <ati@uke.com>
2024-09-13 17:20:28 +0800

    ////

---
+++ NodejsConsoleApp1wzReact/st.bat
@@ -0,0 +1,17 @@
+
+
+rem# C:\Program\nodejs\npm install -g create-react-app
+rem C:\Program\nodejs\npx.cmd  create-react-app reactapp1
+
+rem git config --global user.email "ati@uke.com"
+rem git config --global user.name "ati"
+rem start wbsvr
+
+cd C:\0oprj\reactprj1\reactapp1
+npm start
+
+
+rem----------- back svr
+#rem back svr
+
+rem node  ./reactapp1/src/svr.js
