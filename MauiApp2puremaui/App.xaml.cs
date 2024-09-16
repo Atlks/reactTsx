6cb87bdf25d82148abfb544b25a79b07870e4e2b
ati <ati@uke.com>
2024-09-16 14:21:14 +0800

    maui

---
+++ MauiApp2puremaui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiApp2puremaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
